9743f924d8a0484a2c5d5bbc2ffb4ca47c9b3d87
holyrootx <to_be_it_dinosaur@naver.com>
2024-10-21 11:47:55 +0900

    feat: BEANS파일 내부 네이밍 규칙 수정, 생성

---
+++ src/main/java/model/BoardViewData.java
@@ -0,0 +1,111 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+
+public class BoardViewData {
+
+	//	id,title,contents,author_id,created_at,updated_at,deleted_at,is_deleted,views,likes,hates
+	//  추가적으로 유저의나이, 유저의 성별
+	//  사용자가 글을 볼 때 필요한 데이터들 모음
+	private int id;
+	private String title;
+	private String content;
+	private String gender;
+	private LocalDate birthday;
+	private int author_id;
+	private LocalDateTime created_at;
+	private LocalDateTime updated_at;
+	private LocalDateTime deleted_at;
+	private Boolean is_updated;
+	private Boolean is_deleted;
+	private int view_count;
+	private int like_count;
+	private int hate_count;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
+	public int getAuthor_id() {
+		return author_id;
+	}
+	public void setAuthor_id(int author_id) {
+		this.author_id = author_id;
+	}
+	public LocalDateTime getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(LocalDateTime created_at) {
+		this.created_at = created_at;
+	}
+	public LocalDateTime getUpdated_at() {
+		return updated_at;
+	}
+	public void setUpdated_at(LocalDateTime updated_at) {
+		this.updated_at = updated_at;
+	}
+	public LocalDateTime getDeleted_at() {
+		return deleted_at;
+	}
+	public void setDeleted_at(LocalDateTime deleted_at) {
+		this.deleted_at = deleted_at;
+	}
+	public Boolean getIs_updated() {
+		return is_updated;
+	}
+	public void setIs_updated(Boolean is_updated) {
+		this.is_updated = is_updated;
+	}
+	public Boolean getIs_deleted() {
+		return is_deleted;
+	}
+	public void setIs_deleted(Boolean is_deleted) {
+		this.is_deleted = is_deleted;
+	}
+	public int getView_count() {
+		return view_count;
+	}
+	public void setView_count(int view_count) {
+		this.view_count = view_count;
+	}
+	public int getLike_count() {
+		return like_count;
+	}
+	public void setLike_count(int like_count) {
+		this.like_count = like_count;
+	}
+	public int getHate_count() {
+		return hate_count;
+	}
+	public void setHate_count(int hate_count) {
+		this.hate_count = hate_count;
+	}
+	
+}
